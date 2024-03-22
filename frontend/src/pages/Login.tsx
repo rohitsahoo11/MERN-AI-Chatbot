3d3474df3cfc616fcca09541b1973c7ecbe4b1fb
Rohit Sahoo <rohitsahoo112003@gmail.com>
2024-03-22 20:26:18 +0530

    Changes

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,8 +2,26 @@ import { Box, Button, Typography } from '@mui/material'
 import { IoIosLogIn } from "react-icons/io";
 import React from 'react'
 import CustomizedInput from '../components/shared/CustomizedInput'
+import toast from 'react-hot-toast';
+import { useAuth } from '../context/AuthContext';
 
-function Login() {
+const Login = () => {
+  const auth = useAuth()
+  const  handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const email = formData.get("email") as  string
+    const password = formData.get("password") as string
+    try {
+      toast.loading("Signing In",{id: 'login'})
+      await auth?.login(email, password)
+      toast.success("Signed In successfully", {id: 'login'})
+    } catch (error) {
+      console.log(error);
+      toast.error("signing In Failed", {id: 'login'})
+      
+    }
+  }
   return (
     <Box width={'100%'} height={'100%'} display='flex' flex={1}>
       <Box padding={8} mt={5} display={{md:'flex', sm:'none', sx:'none'}}>
@@ -18,7 +36,9 @@ function Login() {
       ml={'auto'}
       padding={'2'}
       >
-        <form style={{
+        <form
+          onSubmit={handleSubmit} 
+          style={{
           margin:"auto",
           padding:'30px',
           boxShadow:'10px 10px 20px #000',
