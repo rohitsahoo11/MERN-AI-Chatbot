9d5138459e8e3e0aa134d8b6b041b06a5ef7798e
Rohit Sahoo <rohitsahoo112003@gmail.com>
2024-03-24 23:31:06 +0530

    Changes

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,45 +1,74 @@
-import { ReactNode, createContext, useContext, useEffect, useState } from 'react'
-import { loginUser } from '../helpers/api-communicator'
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import {
+  checkAuthStatus,
+  loginUser,
+  logoutUser,
+  signupUser,
+} from "../helpers/api-communicator";
 
 type User = {
-    name: string,
-    email: string
-}
-
+  name: string;
+  email: string;
+};
 type UserAuth = {
-    isLoggedIn: boolean,
-    user: User | null
-    login: (email: string, password: string)=> Promise<void>
-    signup: (name: string, email: string, password: string)=> Promise<void>
-    logout: ()=> Promise<void> 
-}
+  isLoggedIn: boolean;
+  user: User | null;
+  login: (email: string, password: string) => Promise<void>;
+  signup: (name: string, email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+};
+const AuthContext = createContext<UserAuth | null>(null);
 
-const AuthContext = createContext<UserAuth | null>(null)
-export const AuthProvider = ({ children }: { children: ReactNode }) =>{
-    const [user, setUser] = useState<User|null>(null);
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(()=>{
-        //fetch if the user cookies are valid then skip login
-    },[])
-    const login = async(email: string, password: string)=>{
-        const data = await loginUser(email,password)
-        if(data){
-            setUser({email : data.email, name: data.name})
-            setIsLoggedIn(true)
-        }
+  useEffect(() => {
+    // fetch if the user's cookies are valid then skip login
+    async function checkStatus() {
+      const data = await checkAuthStatus();
+      if (data) {
+        setUser({ email: data.email, name: data.name });
+        setIsLoggedIn(true);
+      }
     }
-    const signup = async(name: string, email: string, password: string)=>{}
-    const logout = async()=>{}
-
-    const value = {
-        login,
-        isLoggedIn,
-        user,
-        logout,
-        signup
+    checkStatus();
+  }, []);
+  const login = async (email: string, password: string) => {
+    const data = await loginUser(email, password);
+    if (data) {
+      setUser({ email: data.email, name: data.name });
+      setIsLoggedIn(true);
     }
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
-}
+  };
+  const signup = async (name: string, email: string, password: string) => {
+    const data = await signupUser(name, email, password);
+    if (data) {
+      setUser({ email: data.email, name: data.name });
+      setIsLoggedIn(true);
+    }
+  };
+  const logout = async () => {
+    await logoutUser();
+    setIsLoggedIn(false);
+    setUser(null);
+    window.location.reload();
+  };
 
-export const useAuth = ()=> useContext(AuthContext)
+  const value = {
+    user,
+    isLoggedIn,
+    login,
+    logout,
+    signup,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
